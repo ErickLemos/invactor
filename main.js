ae8e711b12ec079910ce1667e65b7868bd284ce2
Erick B. Lemos <ericklemoscosta@gmail.com>
2024-01-28 15:22:29 -0300

    refatoração

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {Cluster} from "puppeteer-cluster";
-import attackZtePrincipal from "./modelos/modelo-F673AV9.js"
+import attackZteF673AV9 from "./modelos/modelo-F673AV9.js"
 import attackEG8145V5 from "./modelos/modelo-EG8145V5.js"
 import {ips} from "./ips.js";
 import attackGM620 from "./modelos/modelo-GM620.js";
@@ -30,26 +30,22 @@ import attackF670L from "./modelos/modelo-F670L.js";
 async function attack({page, data: ip}) {
     const localpage = page;
     const endereco = `http://${ip}`;
-    let title = "";
 
-    {
-        await localpage.setViewport({
-            width: 764,
-            height: 928
-        })
-    }
-    {
-        await localpage.goto(endereco);
-        await localpage.waitForTimeout(2000);
-        title = await page.title();
-        console.log(title);
-    }
-    {
-        if (title === "F673AV9") await attackZtePrincipal(page, endereco);
-        if (title === "EG8145V5") await attackEG8145V5(page, endereco);
-        if (title === "GM620") await attackGM620(page, endereco);
-        if (title === "F670L") await attackF670L(page, endereco);
-    }
+    await localpage.setViewport({
+        width: 764,
+        height: 928
+    })
+
+    await localpage.goto(endereco, {
+        waitUntil: 'networkidle2',
+        timeout: 5000
+    });
+
+    const title = await page.title();
+    if (title === "F673AV9") await attackZteF673AV9(page, endereco);
+    if (title === "EG8145V5") await attackEG8145V5(page, endereco);
+    if (title === "GM620") await attackGM620(page, endereco);
+    if (title === "F670L") await attackF670L(page, endereco);
+
 }
-
 
